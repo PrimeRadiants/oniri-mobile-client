a2abf4f7c51545d9fda4104cefb1d31c5601d39b
Shanira <georges.biaux@prime-radiants.com>
2016-08-30 07:42:50 +0200

    adding get user to login controller

--- src/js/login/login.controller.js
+++ src/js/login/login.controller.js
@@ -5,8 +5,15 @@
     .module('app')
     .controller('LoginCtrl', LoginCtrl);
 
-    LoginCtrl.$inject = ['$scope'];
-    function LoginCtrl($scope) {
+    LoginCtrl.$inject = ['$scope', '$http'];
+    function LoginCtrl($scope, $http) {
 
+      $scope.login = login;
+
+      function login() {
+        if($scope.email && $scope.password) {
+          $http.get('https://apis.oniri.io/v1/rest/api/user');
+        }
+      }
     }
 })();
